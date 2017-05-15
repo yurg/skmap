34d419dd473bc350ecfd64ad9a7b92742bb8a617
yurg <yurg@yurg.org>
2017-05-15 20:09:28 +0200

    Better icons

--- map.js
+++ map.js
@@ -1,10 +1,11 @@
   var skmap = L.map('mapid').setView([48.730477, 19.704924], 8.4);
 
   var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-  var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+  var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors. Icons: <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>';
   var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib}); 
   skmap.addLayer(osm);
-  /*
+ 
+ /*
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
       maxZoom: 18,
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <br>' +
@@ -13,6 +14,7 @@
       id: 'mapbox.streets'
   }).addTo(skmap);
 */
+
 
   /*###Add geojson######*/
 
@@ -39,31 +41,66 @@
 
   /* Geocoder  */
   L.Control.geocoder().addTo(skmap);
-
-  /*omnivore kml */
-  omnivore.kml('kupele.kml')
+/*
+ omnivore.kml('rekreacne.kml')
       .on('ready', function(layer) {
-          this.eachLayer(function(layer) {
+            this.eachLayer(function(layer) {
               layer.bindPopup(layer.feature.properties.name + "<br>" + layer.feature.properties.description);
           });
+}).addTo(skmap);
+*/
 
-      }).addTo(skmap);
+/* http://jsfiddle.net/nathansnider/Lo8cmuvt/ */
 
-  omnivore.kml('rekreacne.kml')
-      .on('ready', function(layer) {
-          
+/* SPA */
+ var spaLayer = L.geoJson(null, {
+    onEachFeature: getPopup,
+    pointToLayer: getSpaMarker,
+});
 
-          this.eachLayer(function(layer) {
+var spa = omnivore.kml('kupele.kml', null, spaLayer).addTo(skmap);
+
+/*SKI*/
+ var skiLayer = L.geoJson(null, {
+    onEachFeature: getPopup,
+    pointToLayer: getSkiMarker,
+});
 
 
-              layer.bindPopup(layer.feature.properties.name + "<br>" + layer.feature.properties.description);
-          });
 
-      }).addTo(skmap);
+var ski = omnivore.kml('rekreacne.kml', null, skiLayer).addTo(skmap);
 
-      /* icons */
+var spaIcon = L.icon({
+    iconUrl: 'spa.png',
+    iconSize:     [32, 32], // size of the icon
+    shadowSize:   [0, 0], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, 0],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
- var mount = L.AwesomeMarkers.icon({
-    icon: 'fa-area-chart',
-    markerColor: 'red'
+function getSpaMarker(feature, latlng) {
+  return L.marker(latlng, {
+    icon: spaIcon
   });
+}
+
+var skiIcon = L.icon({
+    iconUrl: 'ski.png',
+    iconSize:     [32, 32], // size of the icon
+    shadowSize:   [0, 0], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, 0],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+function getSkiMarker(feature, latlng) {
+  return L.marker(latlng, {
+    icon: skiIcon
+  });
+}
+
+function getPopup(feature, layer) {
+      layer.bindPopup(layer.feature.properties.name + "<br>" + layer.feature.properties.description);
+}
